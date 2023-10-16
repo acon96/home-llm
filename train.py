01cef3dcd9bf03ffc673313dde57acb2178b89fd
Alex O'Connell <git@alexoconnell.net>
2023-10-15 22:58:44 -0400

    save settings for rev5

--- train.py
+++ train.py
@@ -8,7 +8,12 @@ import numpy as np
 torch.set_default_device("cuda")
 torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
-TRAIN_CTX_SIZE = 512
+TRAIN_CTX_SIZE = 512 # The number of tokens to pad + truncate the input examples to
+BATCH_SIZE = 2 # The simulated "batch size" that we will train on. will tweak gradient accumulations steps
+MICRO_BATCH_SIZE = 2 # The actual batch size that will fit into VRAM on this machine
+TRAINING_EPOCHS = 4 # The number of times to train the model on each example
+LEARNING_RATE_START = 1e-5 # The starting learning rate (speed at which the model trains)
+LEARNING_RATE_SCHEDULE = "cosine" # How fast the learning rate is reduced during training
 
 model = AutoModelForCausalLM.from_pretrained("microsoft/phi-1_5", trust_remote_code=True).to(dtype=torch.bfloat16, device="cuda")
 tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5", trust_remote_code=True)
@@ -30,9 +35,9 @@ tokenized_train_dataset = datasets["train"].map(tokenize_function, remove_column
 tokenized_test_dataset = datasets["test"].map(tokenize_function, remove_columns=datasets["test"].column_names)
 
 training_args = TrainingArguments(
-    per_device_train_batch_size=2,
-    per_device_eval_batch_size=2,
-    gradient_accumulation_steps=1,
+    per_device_train_batch_size=MICRO_BATCH_SIZE,
+    per_device_eval_batch_size=MICRO_BATCH_SIZE,
+    gradient_accumulation_steps=BATCH_SIZE/MICRO_BATCH_SIZE,
     evaluation_strategy="epoch",
     save_strategy="epoch",
     # evaluation_strategy="steps",
@@ -41,12 +46,12 @@ training_args = TrainingArguments(
     # save_steps=1000,
     logging_steps=10,
     output_dir="./models/training",
-    num_train_epochs=4,
+    num_train_epochs=TRAINING_EPOCHS,
     save_total_limit=2,
     dataloader_pin_memory=False,
     report_to="tensorboard",
-    learning_rate=1e-4,
-    lr_scheduler_type="cosine",
+    learning_rate=LEARNING_RATE_START,
+    lr_scheduler_type=LEARNING_RATE_SCHEDULE,
     log_level="info",
     bf16=True,
     bf16_full_eval=True,
@@ -60,20 +65,12 @@ class NoAttentionMaskDataCollator(DataCollatorForLanguageModeling):
 
 data_collator = NoAttentionMaskDataCollator(tokenizer, mlm=False)
 
-accuracy_netric = evaluate.load("accuracy")
-def compute_metrics(eval_pred):
-    logits, labels = eval_pred
-    predictions = np.argmax(logits, axis=-1)
-    return accuracy_netric.compute(predictions=predictions, references=labels)
-
-
 trainer = Trainer(
     model=model,
     args=training_args,
     train_dataset=tokenized_train_dataset,
     eval_dataset=tokenized_test_dataset,
     data_collator=data_collator,
-    compute_metrics=compute_metrics,
 )
 
 checkpoint = None
