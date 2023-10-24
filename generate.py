0e08124260346cf62521c9d5c31438dae726bb2d
Alex O'Connell <git@alexoconnell.net>
2023-10-23 22:31:08 -0400

    make generate.py work

--- generate.py
+++ generate.py
@@ -1,40 +1,52 @@
 import torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, AutoConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer
 
-torch.set_default_device("cuda")
-# base_model = AutoModelForCausalLM.from_pretrained("microsoft/phi-1_5", trust_remote_code=True)
-# phi_tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5", trust_remote_code=True)
-trained_model = AutoModelForCausalLM.from_pretrained("./model/home-llm-rev7.1", trust_remote_code=True)
-trained_tokenizer = AutoTokenizer.from_pretrained(
-    "./model/home-llm-rev7.1/tokenizer.json", trust_remote_code=True,
-    config=AutoConfig.from_pretrained("./model/home-llm-rev7.1"))
+SYSTEM_PROMPT = "You are 'Al', a helpful AI Assistant that controls the devices in a house. Complete the following task ask instructed with the information provided only."
 
-def generate(model, tokenizer, inputs):
+def tokenize(tokenizer, prompt):
+    return tokenizer(prompt, return_tensors="pt", return_attention_mask=False)
+
+def generate(model, tokenizer, prompt):
+    inputs = tokenize(tokenizer, prompt)
     outputs = model.generate(**inputs, max_length=512)
     text = tokenizer.batch_decode(outputs)[0]
     return text
 
-def tokenize(tokenizer, question):
-    prompt = f"{question}\nAnswer: "
-    return tokenizer(prompt, return_tensors="pt", return_attention_mask=False)
+def format_example(example):
+    sys_prompt = SYSTEM_PROMPT
+    services_block = "Services: " + ", ".join(sorted(example["available_services"]))
+    states_block = "Devices:\n" + "\n".join(example["states"])
+    question = "Request:\n" + example["question"]
+    response_start = "Response:\n"
+
+    return "\n".join([sys_prompt, services_block, states_block, question, response_start])
+
+def main():
+    request = "turn on the office lights"
+    model_folder = "./model/home-llm-rev7.1"
+
+    example = {
+        "states": [
+            "light.kitchen_sink = on",
+            "light.kitchen_lamp = on",
+            "light.office_desk_lamp = on",
+            "light.family_room_overhead = on",
+            "fan.family_room = off",
+            "lock.fron_door = locked"
+        ],
+        "available_services": ["turn_on", "turn_off", "toggle", "lock", "unlock" ],
+        "question": request,
+    }
+
+    prompt = format_example(example)
+
+    torch.set_default_device("cuda")
+    trained_model = AutoModelForCausalLM.from_pretrained(model_folder, trust_remote_code=True)
+    trained_tokenizer = AutoTokenizer.from_pretrained(model_folder, trust_remote_code=True)
+
+    output = generate(trained_model, trained_tokenizer, prompt)
+    print(output)
 
 
-# def compare(question):
-#     tokenized_prompt = tokenize(phi_tokenizer, question)
-#     print("--------------------------------------------------------------------")
-#     print("base model:")
-#     print(generate(base_model, phi_tokenizer, tokenized_prompt))
-#     print("--------------------------------------------------------------------")
-#     print("fine tuned model:")
-#     print(generate(trained_model, phi_tokenizer, tokenized_prompt))
-
-# prompt1 = '''My brother is 10 years older than me when I am 3 years younger than my sister who is 7. In 3 years how old will I be?'''
-# prompt2 = '''Find the vertex of the parabola defined by the function y = 4x^2 + 8x - 2'''
-# prompt3 = '''Joe has 3 pairs of shoes that he got for his birthday. If Joe can sell one pair of shoes for $200 but has to pay 10% tax on each sale, how much money will Joe get if he sells 2 of the pairs of shoes and keeps the last pair for himself?'''
-
-# compare(prompt1)
-# compare(prompt2)
-
-prompt = "You are 'Al', a helpful AI Assistant that controls the devices in a house. Complete the following task ask instructed with the information provided only.\nServices: close_cover, decrease_speed, increase_speed, open_cover, stop_cover, toggle, turn_off, turn_on, unlock\nDevices:\nlight.upstairs_basement_zwave = on\nlight.upstairs_utility_zigbee = off\nfan.dining_area = on\nlight.back_arcade_warm = on\nlight.downstairs_sunroom_homekit = on\nblinds.patio = open\ngarage_door.basement = closed\nlight.side_hallway_hue = on\nlight.patio_1 = off\nlight.front_den_ge = on\nlight.kitchen_trashbin_cool = on\nlight.deck_left = on\nfan.honeywell_turbo = on\nlight.kitchen_floor_cool = on\nfan.living_room_center = on\nlight.kitchen_microwave_warm = on\nlight.front_arcade_ge = off\nlock.office = locked\nfan.ceiling_1 = on\nlight.kitchen_toaster_cool = on\nlight.office_2 = on\nlight.kitchen_sideboard_cool = off\nlight.upstairs_playroom_zwave = on\nlight.kitchen_sink_cool = on\ngarage_door.golf_cart = open\ngarage_door.main_1 = closed\nlight.upstairs_observatory_zigbee = on\nlight.garden_2 = off\nlight.patio = on\nlight.garden = off\nfan.study = on\nlight.kitchen_1 = off\nRequest: please unlock the office\nResponse:"
-
-generate(trained_model, trained_tokenizer, trained_tokenizer(prompt, return_tensors="pt", return_attention_mask=False))
+if __name__ == "__main__":
+    main()
