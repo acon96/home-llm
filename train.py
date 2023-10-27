abd2818a2b9d1f7bdaf5f60b5f99c80679287bbc
Alex O'Connell <git@alexoconnell.net>
2023-10-26 21:27:54 -0400

    add lora + argument parsing to trainer

--- train.py
+++ train.py
@@ -1,47 +1,93 @@
+import os
 import torch
-import torch.nn.functional
-from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer, DataCollatorForLanguageModeling
+from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments, Trainer, DataCollatorForLanguageModeling, HfArgumentParser
 from datasets import load_dataset
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
-torch.set_default_device("cuda")
-torch.set_default_tensor_type('torch.cuda.FloatTensor')
+# torch.set_default_device("cuda")
+# torch.set_default_tensor_type('torch.cuda.FloatTensor')
 
-# TODO: convert this in to cmd line args
-TRAIN_CTX_SIZE = 512 # The number of tokens to pad + truncate the input examples to
-BATCH_SIZE = 8 # The simulated "batch size" that we will train on. will tweak gradient accumulations steps
-MICRO_BATCH_SIZE = 2 # The actual batch size that will fit into VRAM on this machine
-TRAINING_EPOCHS = 1 # The number of times to train the model on each example
-LEARNING_RATE_START = 1e-5 # The starting learning rate (speed at which the model trains)
-LEARNING_RATE_SCHEDULE = "cosine" # How fast the learning rate is reduced during training
-RUN_NAME = "home-llm-rev8.9"
-OUTPUT_DIR =f"./models/{RUN_NAME}"
+"""
+python3 train.py \
+    --run_name home-llm-revXX \
+    --base_model microsoft/phi-1_5
+    --train_dataset data/home_assistant_train.json \
+    --test_dataset data/home_assistant_test.json
+"""
+
+@dataclass
+class TrainingRunArguments:
+    base_model: str = field(metadata={"help": "The base model to load for fine-tuning"})
+    ctx_size: int = field(default=512, metadata={"help": "The number of tokens to pad & truncate the input examples to"})
+    bf16: bool = field(default=True, metadata={"help": "If set, the model will the loaded and trained in bf16 instead of fp16"})
+    batch_size: int = field(default=8, metadata={"help": "The simulated 'batch size' that we will train on. will tweak gradient accumulations steps"})
+    micro_batch_size: int = field(default=2, metadata={"help": "The actual batch size that will fit into VRAM on this machine"})
+    epochs: int = field(default=1, metadata={"help": "The number of times to train the model on each example"})
+    learning_rate: float = field(default=1e-5, metadata={"help": "The starting learning rate (speed at which the model trains)"})
+    learning_rate_schedule: str = field(default="cosine", metadata={"help": "How fast the learning rate is reduced during training"})
+    run_name: str = field(metadata={"help": "The folder to save the output model under"})
+    train_dataset: str = field(metadata={"help": "The JSON file containing the training dataset"})
+    test_dataset: str = field(metadata={"help": "The JSON file containing the evaluation dataset"})
+
+    use_lora: bool = field(default=False, metadata={"help": "If set, then the trained model will be a LoRA"})
+    lora_rank: int = field(default=4)
+    lora_alpha: int = field(default=32)
+    lora_dropout: float = field(default=0.05)
+
+parser = HfArgumentParser([TrainingRunArguments])
+training_run_args, _ = parser.parse_args_into_dataclasses(return_remaining_strings=True)
 
 # TODO: write a proper evaluation script
 
-model = AutoModelForCausalLM.from_pretrained("microsoft/phi-1_5", trust_remote_code=True).to(dtype=torch.bfloat16, device="cuda")
-tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5", trust_remote_code=True)
-# tokenizer.pad_token = tokenizer.eos_token
-# pad_token_id = tokenizer(tokenizer.pad_token)["input_ids"][0]
+model = AutoModelForCausalLM.from_pretrained(training_run_args.base_model, trust_remote_code=True, torch_dtype=torch.bfloat16)
+tokenizer = AutoTokenizer.from_pretrained(training_run_args.base_model, trust_remote_code=True)
 tokenizer.add_special_tokens({'pad_token': '<|pad|>'})
-# model.resize_token_embeddings(len(tokenizer), pad_to_multiple_of=8)
+
+training_callbacks = []
+if training_run_args.use_lora:
+    from peft import LoraConfig, TaskType, get_peft_model
+
+    class SavePeftModelCallback(transformers.TrainerCallback):
+
+        def on_save(self, args, state, control, **kwargs):
+            checkpoint_folder_name = f"{transformers.trainer_utils.PREFIX_CHECKPOINT_DIR}-{state.global_step}"
+            checkpoint_folder = os.path.join(args.output_dir, checkpoint_folder_name)
+
+            peft_model_path = os.path.join(checkpoint_folder, "adapter_model")
+            kwargs["model"].save_pretrained(peft_model_path)
+
+            return control
+        
+    training_callbacks.append(SavePeftModelCallback)
+
+    peft_config = LoraConfig(
+        task_type=TaskType.CAUSAL_LM,
+        inference_mode=False,
+        r=training_run_args.lora_rank,
+        lora_alpha=training_run_args.alpha,
+        lora_dropout=training_run_args.droput,
+        target_modules=None,
+    )
+
+    model = get_peft_model(model, peft_config)
+    model.print_trainable_parameters()
 
 def tokenize_function(example):
     result = tokenizer(example['text'] + tokenizer.eos_token,
                        return_attention_mask=False,
-                       padding=True, max_length=TRAIN_CTX_SIZE, truncation=True)
+                       padding=True, max_length=training_run_args.ctx_size, truncation=True)
     
     return result
 
 
-datasets = load_dataset("json", data_files={ "train": "data/home_assistant_train.json", "test": "data/home_assistant_test.json" })
+datasets = load_dataset("json", data_files={  "train": training_run_args.train_dataset, "test": training_run_args.test_dataset })
 tokenized_train_dataset = datasets["train"].map(tokenize_function, remove_columns=datasets["train"].column_names)
 tokenized_test_dataset = datasets["test"].map(tokenize_function, remove_columns=datasets["test"].column_names)
 
 training_args = TrainingArguments(
-    per_device_train_batch_size=MICRO_BATCH_SIZE,
-    per_device_eval_batch_size=MICRO_BATCH_SIZE,
-    gradient_accumulation_steps=BATCH_SIZE/MICRO_BATCH_SIZE,
+    per_device_train_batch_size=training_run_args.micro_batch_size,
+    per_device_eval_batch_size=training_run_args.micro_batch_size,
+    gradient_accumulation_steps=training_run_args.batch_size/training_run_args.micro_batch_size,
     evaluation_strategy="epoch",
     save_strategy="epoch",
     # evaluation_strategy="steps",
@@ -49,16 +95,17 @@ training_args = TrainingArguments(
     # eval_steps=1000,
     # save_steps=1000,
     logging_steps=5,
-    output_dir=OUTPUT_DIR,
-    num_train_epochs=TRAINING_EPOCHS,
+    output_dir=f"./models/{training_run_args.run_name}",
+    num_train_epochs=training_run_args.epochs,
     save_total_limit=1,
     dataloader_pin_memory=False,
     report_to="tensorboard",
-    learning_rate=LEARNING_RATE_START,
-    lr_scheduler_type=LEARNING_RATE_SCHEDULE,
+    learning_rate=training_run_args.learning_rate,
+    lr_scheduler_type=training_run_args.learning_rate_schedule,
     log_level="info",
-    bf16=True,
-    bf16_full_eval=True,
+    bf16=training_run_args.bf16,
+    bf16_full_eval=training_run_args.bf16,
+    callbacks=training_callbacks,
 )
 
 class NoAttentionMaskDataCollator(DataCollatorForLanguageModeling):
@@ -69,14 +116,13 @@ class NoAttentionMaskDataCollator(DataCollatorForLanguageModeling):
 
 data_collator = NoAttentionMaskDataCollator(tokenizer, mlm=False)
 
-# TODO: ignore user input when training
+# TODO: ignore user input when training by masking the input properly
 # @dataclass
 # class CustomDataCollator:
 #     tokenizer: AutoTokenizer
 #     train_ctx_size: int
 #     def __call__(self, features, **kwargs):
 #         for feature in features:
-
 # data_collator = CustomDataCollator(tokenizer=tokenizer)
 
 trainer = Trainer(
@@ -84,8 +130,6 @@ trainer = Trainer(
     args=training_args,
     train_dataset=tokenized_train_dataset,
     eval_dataset=tokenized_test_dataset,
-    # train_dataset=datasets["train"],
-    # eval_dataset=datasets["test"],
     data_collator=data_collator,
 )
 
@@ -96,5 +140,6 @@ if checkpoint:
 else:
     trainer.train()
 
+trainer.save_state()
 trainer.save_model()
-tokenizer.save_pretrained(OUTPUT_DIR)
+tokenizer.save_pretrained(f"./models/{training_run_args.run_name}")
