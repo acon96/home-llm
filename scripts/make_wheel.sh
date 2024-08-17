1d50c3f589ab53bb3b50569fe48ae42facc14f11
Alex O'Connell <git@alexoconnell.net>
2024-08-17 00:00:41 -0400

    fix wheel creation + bump llama.cpp version again

--- scripts/make_wheel.sh
+++ scripts/make_wheel.sh
@@ -2,11 +2,15 @@
 # Don't run this. This is executed inside of the home assistant container to build the wheel
 
 apk update
-apk add build-base python3-dev
+apk add build-base python3-dev cmake
 
 cd /tmp
 git clone --recurse-submodules https://github.com/abetlen/llama-cpp-python --branch $1
 cd llama-cpp-python
 pip3 install build
+
+# for some reason, scikit-build-core v0.9.7+ doesn't produce properly tagged musllinux wheels
+sed -i -e 's/scikit-build-core\[pyproject\]>=0.9.2/scikit-build-core\[pyproject\]==0.9.6/g' pyproject.toml
+
 python3 -m build --wheel
 cp -f ./dist/*.whl /tmp/dist/
