b2b81c419a5ebd39fa8356ae32c2990251dc60fa
Witold Greń <witold.gren@gmail.com>
2024-08-20 17:24:26 +0200

    added tokenize example and also added prefix and suffix tokens without leading whitespace

--- find_split.py
+++ find_split.py
@@ -26,6 +26,24 @@ print("-" * 100)
 print(assistant_prompt)
 print("-" * 100)
 
+# Added real example to test the tokenizer
+assistant_prompt_tokens = tokenizer.apply_chat_template(
+    conversation=[
+        {"role": "system", "content": r"Jeste\u015b \u201eAl\u201d, pomocnym asystentem AI, kt\u00f3ry kontroluje urz\u0105dzenia w domu. Wykonaj poni\u017csze zadanie zgodnie z instrukcj\u0105 lub odpowiedz na poni\u017csze pytanie, korzystaj\u0105c wy\u0142\u0105cznie z podanych informacji.\nAktualna godzina i data to 16:42 w niedziela, 18 marca 2029\nUs\u0142ugi: cover.close_cover(), cover.open_cover(), cover.stop_cover(), cover.toggle(), fan.decrease_speed(), fan.increase_speed(), fan.toggle(), fan.turn_off(), fan.turn_on(), cover.close_cover(), cover.open_cover(), cover.stop_cover(), cover.toggle(), light.toggle(), light.turn_off(), light.turn_on(rgb_color,brightness), lock.lock(), lock.unlock(), media_player.media_next_track(), media_player.media_pause(), media_player.media_play(), media_player.media_play_pause(), media_player.media_previous_track(), media_player.media_stop(), media_player.toggle(), media_player.turn_off(), media_player.turn_on(), media_player.volume_down(), media_player.volume_mute(), media_player.volume_up(), switch.toggle(), switch.turn_off(), switch.turn_on()\nUrz\u0105dzenia:\nlight.g\u00f3ra_gabinet_zwave '\u015awiat\u0142o w gabinecie na pi\u0119trze' = on;saddlebrown (114, 71, 52);69%\nlight.prz\u00f3d_korytarz_homekit '\u015awiat\u0142o w przednim korytarzu' = off;seagreen (47, 111, 119);3%\nswitch.master_bedroom_lights 'G\u0142\u00f3wny w\u0142\u0105cznik \u015bwiate\u0142 w sypialni' = off\nmedia_player.roku_sypialnia 'Odtwarzacz Roku w sypialni' = off\ncover.wiata_samochodowa 'Drzwi do wiaty' = closed\nlight.\u0142azienka_2 '\u015awiat\u0142o w \u0142azience' = off;tan (216, 174, 142);97%\nlock.gara\u017c 'Zamek drzwi gara\u017cowych' = locked\nlock.drzwi_dachowe 'Zamek dost\u0119pu na dachu' = locked\nlock.szopa_2 'Drugi zamek szopy' = unlocked\nlight.g\u0142\u00f3wna_sypialnia_1 '\u015awiat\u0142o w g\u0142ownej sypialni' = off\nlock.drzwi_ganku 'Zamek drzwi werandy' = locked\ncover.szopa_2 'Drugie drzwi do szopy' = closed\nlock.pok\u00f3j_kartowania 'Zamek do pokoju akt' = unlocked\nlight.gara\u017c_osram '\u015awiat\u0142o w Gara\u017cu Osram' = on;purple (148, 40, 119)\nmedia_player.home_theater_system_living_room 'Kino domowe w salonie' = off\ncover.kuchnia 'Kuchnia' = open\nlock.szklarnia 'Zamek szklarni' = locked\nlock.sypialnia_3 'Zamek w trzeciej sypialni' = locked\nfan.ganek 'Wentylator sufitowy na werandzie' = off\nlight.tylna_sypialnia_ciep\u0142e 'Tylna sypialnia Ciep\u0142e \u015bwiat\u0142o' = off;mediumslateblue (125, 83, 236);81%\nswitch.\u015bwiat\u0142a_warsztatowe 'Prze\u0142\u0105cznik \u015bwiate\u0142 warsztatowych' = off\nlight.philips_hue_2 'Philips Hue' = on;darkseagreen (161, 205, 137);72%\ncover.ikea_smart 'Inteligentne rolety z Ikei' = open"},
+        {"role": "user", "content": r"opu\u015b\u0107 \u017caluzje kuchenne"},
+        {"role": "assistant", "content": r"zamykam rolety, jak prosi\u0142e\u015b.\n```homeassistant\n{\"service\": \"cover.close_cover\", \"target_device\": \"cover.kuchnia\"}\n```"}
+    ],
+    tokenize=True,
+    truncation=True,
+    max_length=2048,
+    add_generation_prompt=False
+)
+
+print("Chat template tokens:")
+print("-" * 100)
+print(assistant_prompt_tokens)
+print("-" * 100)
+
 response_prefix = assistant_prompt.split(r"HA_REQUEST")[1].split(r"HA_RESPONSE")[0]
 response_suffix = assistant_prompt.split(r"HA_RESPONSE")[1]
 
@@ -46,12 +64,16 @@ suffix_ids2 = tokenizer(" " + response_suffix, add_special_tokens=False)["input_
 prefix_ids3 = tokenizer("\n" + response_prefix, add_special_tokens=False)["input_ids"]
 suffix_ids3 = tokenizer("\n" + response_suffix, add_special_tokens=False)["input_ids"]
 
+prefix_ids4 = tokenizer(response_prefix.strip(), add_special_tokens=False)["input_ids"]
+suffix_ids4 = tokenizer(response_suffix.strip(), add_special_tokens=False)["input_ids"]
+
 print(f"Estimated tokens for {model}")
 print("response prefix:")
 print(response_prefix)
 print("tokens with no leading whitespace:", prefix_ids)
 print("tokens with leading whitespace:", prefix_ids2)
 print("tokens with leading newline:", prefix_ids3)
+print("tokens without leading whitespace:", prefix_ids4)
 
 print("---------------")
 
@@ -60,6 +82,7 @@ print(response_suffix)
 print("tokens with no leading whitespace:", suffix_ids)
 print("tokens with leading whitespace:", suffix_ids2)
 print("tokens with leading newline:", suffix_ids3)
+print("tokens without leading whitespace:", suffix_ids4)
 
 
 def _find_mask_ranges(input_ids, prefix_ids, suffix_ids):
