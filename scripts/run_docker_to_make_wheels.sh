ed5312e9ffbf81351f8d520d0e9c2de409263d2d
Alex O'Connell <git@alexoconnell.net>
2024-12-15 10:51:56 -0500

    update llama cpp python and build python 13 wheels now

--- scripts/run_docker_to_make_wheels.sh
+++ scripts/run_docker_to_make_wheels.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
-VERSION_TO_BUILD="v0.2.88"
+VERSION_TO_BUILD="v0.3.5"
 
-# make python11 wheels
+# make python 11 wheels
 # docker run -it --rm \
 #     --entrypoint bash \
 #     -v $(pwd):/tmp/dist \
 #     homeassistant/home-assistant:2023.12.4 /tmp/dist/make_wheel.sh $VERSION_TO_BUILD
 
 # make python 12 wheels
+# docker run -it --rm \
+#     --entrypoint bash \
+#     -v $(pwd):/tmp/dist \
+#     homeassistant/home-assistant:2024.2.1 /tmp/dist/make_wheel.sh $VERSION_TO_BUILD
+
+# make python 13 wheels
 docker run -it --rm \
     --entrypoint bash \
     -v $(pwd):/tmp/dist \
-    homeassistant/home-assistant:2024.2.1 /tmp/dist/make_wheel.sh $VERSION_TO_BUILD
+    homeassistant/home-assistant:2024.12 /tmp/dist/make_wheel.sh $VERSION_TO_BUILD
